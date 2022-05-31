c20ee605c3164594aa2b9c2138d9e632f76a7d90
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-05-31 20:20:58 +0530

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import ClassCounter from "./components/hooks/useState/ClassCounter";
-import ClassCounterTwo from "./components/hooks/useState/ClassCounterTwo";
-import HookCounter from "./components/hooks/useState/HookCounter";
-import HookCounterTwo from "./components/hooks/useState/HookCounterTwo";
-import HookCounterFour from "./components/hooks/useState/UseStateWithArray";
-import HookCounterThree from "./components/hooks/useState/UseStateWithObject";
+// import ClassCounter from "./components/hooks/useState/ClassCounter";
+// import ClassCounterTwo from "./components/hooks/useState/ClassCounterTwo";
+// import HookCounter from "./components/hooks/useState/HookCounter";
+// import HookCounterTwo from "./components/hooks/useState/HookCounterTwo";
+// import HookCounterFour from "./components/hooks/useState/UseStateWithArray";
+// import HookCounterThree from "./components/hooks/useState/UseStateWithObject";
+// import ConditinalRunEffect from "./components/useEffect/ClassConditinal";
+// import ConditinallyRunEffect from "./components/useEffect/ConditinallyRunEffect";
+import ClassMouse from "./components/useEffect/ClassRunEffectonlyonce";
 import "./styles.css";
 
 export default function App() {
@@ -13,8 +16,11 @@ export default function App() {
       <HookCounter />
       <HookCounterTwo />
       <ClassCounterTwo /> */}
-      <HookCounterThree />
-      <HookCounterFour />
+      {/* <HookCounterThree />
+      <HookCounterFour /> */}
+      {/* <ConditinalRunEffect />
+      <ConditinallyRunEffect /> */}
+      <ClassMouse />
     </div>
   );
 }
